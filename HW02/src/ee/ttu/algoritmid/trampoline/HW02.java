f7b5e1fd856d0abdc1e053a094b82777c2e11fbf
kaviik <kaviik@ttu.ee>
2019-11-28 18:43:45 +0200

    Checkpoint or something

--- HW02/src/ee/ttu/algoritmid/trampoline/HW02.java
+++ HW02/src/ee/ttu/algoritmid/trampoline/HW02.java
@@ -1,13 +1,73 @@
 package ee.ttu.algoritmid.trampoline;
 
-import ee.ttu.algoritmid.trampoline.TrampolineCenter;
-
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class HW02 implements TrampolineCenter {
 
+	private static List<String> dijkstraEqual(int[][] map) {
+		if (map.length == 1) {
+			return new ArrayList<>();
+		}
+		int size = map.length;  // edge length of the map
+		int[][] dist = new int[size][size];  // array for storing distances for corresponding map squares
+		int[][][] from = new int[size][size][2];  // array for storing the coordinates from which this coordinate was reached from
+		ArrayDeque<int[]> queue = new ArrayDeque<>();  // queue for storing next node
+		queue.add(new int[]{0, 0});  // throw starting point to queue
+		while (!queue.isEmpty()) {
+			int[] coords = queue.poll();
+			int jump = map[coords[0]][coords[1]];  // jump value of the jumpy jump
+			int newFirst = coords[0] + jump;  // new first coordinate
+			int newSecond = coords[1] + jump;  // new second coordinate
+			if (newFirst < size && dist[newFirst][coords[1]] == 0) {  // if not out of bounds and node not visited
+				from[newFirst][coords[1]] = coords;  // save the current coords to the from of new coords
+				dist[newFirst][coords[1]] = dist[coords[0]][coords[1]] + 1;
+				queue.add(new int[]{newFirst, coords[1]});  // add this new node to the queue
+				if (newFirst == (size - 1) && coords[1] == (size - 1)) {  // if has reached the end, break out
+					break;
+				}
+			}
+			if (newSecond < size && dist[coords[0]][newSecond] == 0) {  // if not out of bounds and node not visited
+				from[coords[0]][newSecond] = coords;  // save the current coords to the from of new coords
+				dist[coords[0]][newSecond] = dist[coords[0]][coords[1]] + 1;
+				queue.add(new int[]{coords[0], newSecond});  // add this new node to the queue
+				if (coords[0] == (size - 1) && newSecond == (size - 1)) {  // if has reached the end, break out
+					break;
+				}
+			}
+		}
+		System.out.println("I reached");
+		if (!queue.isEmpty()) {  // solution was found
+			LinkedList<String> answer = new LinkedList<>();
+			int[] coords = new int[]{size - 1, size - 1};
+			while (true) {
+				int[] fromcoords = from[coords[0]][coords[1]];
+				if (fromcoords[0] == coords[0]) {
+					answer.push("S" + (coords[1] - fromcoords[1]));
+				}
+				else {
+					answer.push("E" + (coords[0] - fromcoords[0]));
+				}
+				if (fromcoords[0] == 0 && fromcoords[1] == 0) {
+					break;
+				}
+			}
+			return answer;
+		}
+		else {  // solution was not found, null I guess
+			return null;
+		}
+	}
+
 	@Override
 	public List<String> findMinJumps(int[][] map) {
-		return null;
+		return dijkstraEqual(map);
+	}
+
+	public static void main(String[] args) {
+		int[][] map = new int[][]{{1, 1}, {1, 1}};
+		System.out.println(dijkstraEqual(map));
 	}
 }
