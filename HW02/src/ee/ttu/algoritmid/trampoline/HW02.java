8781acfcf6a134f74bd60e8cf1f164271da5f8cb
envomp <envomp@taltech.ee>
2019-11-29 15:39:37 +0200

    Dijkstra

--- HW02/src/ee/ttu/algoritmid/trampoline/HW02.java
+++ HW02/src/ee/ttu/algoritmid/trampoline/HW02.java
@@ -296,7 +296,7 @@ public class HW02 implements TrampolineCenter {
 
     @Override
     public List<String> findMinJumps(int[][] map) {
-        return floodFill(map);
+        return dijkstraApproxV1(map);
     }
 
     public static void main(String[] args) {
